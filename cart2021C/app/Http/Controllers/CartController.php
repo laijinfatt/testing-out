90e223645ffd2114aa2ad985324719264ce41279
laijinfatt <jinfatt124192@gmail.com>
2021-12-17 13:18:24 +0800

    remove cart item

--- cart2021C/app/Http/Controllers/CartController.php
+++ cart2021C/app/Http/Controllers/CartController.php
@@ -36,4 +36,12 @@ class CartController extends Controller
 
         return view('myCart')->with('carts',$carts);
     }
+
+    public function delete($id){
+
+        $deleteItem=myCart::find($id); //binding record
+        $deleteItem->delete(); //delete record
+        Session::flash('success',"Item was removed successfully!");
+        return redirect()->route('show.my.cart');
+    }
 }
